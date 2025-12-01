007044acf164efd7bddfdcb0fea9890db38a87d0
ItzDast <127223851+ItzDast@users.noreply.github.com>
2025-12-01 23:19:37 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const style = document.createElement('style');
-style.textContent = `
-  @media (min-width: 900px) {
-    .content__inner {
-      margin: auto 0px ${(document.querySelector('.content').offsetHeight - document.querySelector('.content__inner').offsetHeight + document.querySelector('.header__inner').offsetHeight) / 2}px;
-    }
-  }
-`;
-document.head.appendChild(style);
+// const style = document.createElement('style');
+// style.textContent = `
+//   @media (min-width: 900px) {
+//     .content__inner {
+//       margin: auto 0px ${(document.querySelector('.content').offsetHeight - document.querySelector('.content__inner').offsetHeight + document.querySelector('.header__inner').offsetHeight) / 2}px;
+//     }
+//   }
+// `;
+// document.head.appendChild(style);
 
 // сверху выравнивает по центру content__inner
 
